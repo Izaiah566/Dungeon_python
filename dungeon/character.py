e89b678e2216d89ebb7caf07af73d1abdca28e63
Izaiah <izaiahlharrison@gmail.com>
2025-05-16 16:41:13 -0400

    initial commit

--- dungeon/character.py
+++ dungeon/character.py
@@ -1,16 +1,44 @@
 import pygame
+import math
 import constants
 
 
 
 class Character():
-    def __init__(self, x, y):
+    def __init__(self, x, y, animated_player_images):
+        self.animated_player_images = animated_player_images
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        self.image = animated_player_images[self.frame_index]
+        self.flip = False
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
     def move(self, deltaX, deltaY):
+        if deltaX < 0:
+            self.flip = True
+        elif deltaX < 0:
+            self.flip = False
+
+        # diagonal movement speed
+        if deltaX != 0 and deltaY != 0: # press buttons at the same time
+            deltaX *= (math.sqrt(2) / 2)
+            deltaY *= (math.sqrt(2) / 2)
+
         self.rect.x += deltaX
         self.rect.y += deltaY
 
+    def update(self):
+        animation_cooldown = 100
+        now = pygame.time.get_ticks()
+        if now - self.update_time > animation_cooldown:
+            self.update_time = now
+            self.frame_index += 1
+            if self.frame_index >= len(self.animated_player_images):
+                self.frame_index = 0
+            self.image = self.animated_player_images[0][self.frame_index]
+
     def draw_player(self, screen):
-        pygame.draw.rect(screen, (0, 255, 0), self.rect)
+        flipped_image = pygame.transform.flip(self.image, self.flip, False)
+        screen.blit(flipped_image, self.rect)
+        pygame.draw.rect(screen, constants.RED, self.rect, 1)
--- dungeon/main.py
+++ dungeon/main.py
@@ -2,12 +2,10 @@ import pygame
 import constants
 from character import Character
 
-pygame.init()
-
-
-
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon Game")
+
+# Create clock for FPS
 clock = pygame.time.Clock()
 
 # define character movements
@@ -16,35 +14,53 @@ moving_right = False
 moving_up = False
 moving_down = False
 
+# scale function
+def scale_image(image, scale):
+    return pygame.transform.scale(image, (image.get_width() * scale, image.get_height() * scale))
+
+animation_types = ["standing", "running"]
+animated_player_images = []
+for animation_type in animation_types:
+    temp_list = []
+    for i in range(4):
+        player_image =  pygame.image.load(f"/Users/izaiahharrison/Repos/python_pygame/dungeon/assets/character_0/{animation_type}/{i}.png")
+        player_image = scale_image(player_image, constants.SCALE)
+        temp_list.append(player_image)
+    animated_player_images.append(temp_list)
+
 # Create character
-character = Character(100, 100)
+character = Character(100, 100, animated_player_images)
 
 # Game Loop
 running = True
 while running:
 
+    # set FPS
+    clock.tick(constants.FPS)
+
+    # Clear background
+    screen.fill(constants.BACKGROUND_WHITE)
+
     # calculate characters movement delta x and delta y
     deltaX = 0
     deltaY = 0
     if moving_left:
-        deltaX -= 5
+        deltaX -= constants.SPEED
     if moving_right:
-        deltaX += 5
+        deltaX += constants.SPEED
     if moving_up:
-        deltaY -= 5
+        deltaY -= constants.SPEED
     if moving_down:
-        deltaY += 5
+        deltaY += constants.SPEED
 
     # Update character movements
     character.move(deltaX, deltaY)
 
-    print(deltaX, deltaY)
-
     # draw character on screen
     character.draw_player(screen)
 
-    # set FPS
-    clock.tick(constants.FPS)
+    # Update character
+    character.update()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
